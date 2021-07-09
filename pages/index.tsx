348c3dc8c554c9b50e7bf2af4914918015bb1483
wndererk <bangwan.ap@hotmail.com>
2021-07-09 11:15:25 +0700

    using React.Fragment in index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Head from 'next/head'
+//import { withTranslation } from '../i18n';
 
 const Homepage = ({t}:any) => {
   return(
-    <>
-    <h1>Hi</h1>
-    </>
+    <React.Fragment>
+      <h1>Hi</h1>
+    </React.Fragment>
   )
 }
 
-// export default withTranslation('Home')(Homepage);
+//export default withTranslation('Home')(Homepage);
 // export const getStaticProps = async () => {
 //     return {
 //         props: {
