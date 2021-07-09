f9c1392dc54a7a1667a8414ac8132bb14c742f9c
wndererk <bangwan.ap@hotmail.com>
2021-07-09 15:22:15 +0700

    Add HeroSection

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -3,9 +3,24 @@ import Link from 'next/link';
 
 const HeroSectionV2 = ({ t }: any) => {
     return(
-        <>
-            <h1>HeroSection</h1>
-        </>
+        <div className="hero_section v2">
+            <div className="container">
+                <h1>No.1 Top SMS Marketing Provider</h1>
+                <p>Send advertising SMS via SMS Gateway, Fast, Stable, Ready to give consultation at every step.</p>        
+                <ul className="cta-btn">
+                    <li>
+                        <Link href="">
+                            <a>Try for free</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="">
+                            <a>View packages</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
     )  
 }
 
