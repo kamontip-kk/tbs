9318d7daa7750dad89379fa886bd52a3224365f1
wndererk <bangwan.ap@hotmail.com>
2021-07-12 10:01:55 +0700

    Add GlobeSeection

--- components/home/GlobeSection.tsx
+++ components/home/GlobeSection.tsx
@@ -9,14 +9,14 @@ const GlobeSection = ({ t }: any) => {
                 <div className="col-lg-6 col-md-12 col-12 order-lg-1 order-md-2 order-2">
                     <div className="globe_text">
                         <h2>
-                            Free SMS and OTP sending worldwide at a great value price.
+                            Free SMS and OTP sending worldwide at a great value price
                         </h2>
                         <p>
                             Covering more than 200 countries worldwide
                         </p>
                         <Link href="/contact">
                             <a className="btn v3">
-                                Contact us for more information'
+                                Contact us for more information
                             </a>
                         </Link>
                     </div>
