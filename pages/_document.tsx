b7118d6fa338f4d89f1fade70f8643c02c29e9fd
wndererk <bangwan.ap@hotmail.com>
2021-07-21 17:30:00 +0700

    test: remove all _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,73 +29,73 @@ class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css2?family=Prompt:wght@200;300;400;500;600;700;800&display=swap"
                     /> */}
-                    <link
+                    {/* <link
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;600;700;800&display=swap"
-                    />
+                    /> */}
 
-                    <link
+                    {/* <link
                         href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                         rel="stylesheet"
-                    />
+                    /> */}
                     {/* <link
                         href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.6.2/animate.min.css"
                         rel="stylesheet"
                     /> */}
-                    <link
+                    {/* <link
                         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css"
                         rel="stylesheet"
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.0/css/ionicons.min.css"
                         rel="stylesheet"
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
                         rel="stylesheet"
-                    />
+                    /> */}
                     {/* <link
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/css/nice-select.css`}
                         rel="stylesheet"
                     /> */}
-                    <link
+                    {/* <link
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/css/style.css`}
                         rel="stylesheet"
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/css/responsive.css`}
                         rel="stylesheet"
-                    />
+                    /> */}
                     {/* <link
                         rel="stylesheet"
                         href="https://unpkg.com/swiper/swiper-bundle.min.css"
                     /> */}
-                    <link
+                    {/* <link
                         rel="apple-touch-icon"
                         sizes="57x57"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/apple-icon-57x57.png`}
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         rel="apple-touch-icon"
                         sizes="60x60"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/apple-icon-60x60.png`}
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         rel="apple-touch-icon"
                         sizes="72x72"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/apple-icon-72x72.png`}
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         rel="apple-touch-icon"
                         sizes="76x76"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/apple-icon-76x76.png`}
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         rel="apple-touch-icon"
                         sizes="114x114"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/apple-icon-114x114.png`}
-                    />
-                    <link
+                    /> */}
+                    {/* <link
                         rel="apple-touch-icon"
                         sizes="120x120"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/apple-icon-120x120.png`}
@@ -138,7 +138,7 @@ class MyDocument extends Document {
                         type="image/png"
                         sizes="16x16"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/favicon-16x16.png`}
-                    />
+                    /> */}
                     {/* <link
                         rel="manifest"
                         href={`${process.env.NEXT_PUBLIC_BASE_ASSET}/img-fav/manifest.json`}
@@ -153,14 +153,14 @@ class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
-                    <script
+                    {/* <script
                         defer
                         src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"
                     ></script>
                     <script
                         defer
                         src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-                    ></script>
+                    ></script> */}
                     {/* <script
                         defer
                         src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
@@ -169,7 +169,7 @@ class MyDocument extends Document {
                         defer
                         src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"
                     ></script> */}
-                    <script
+                    {/* <script
                         defer
                         src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/plugins/bgset/ls.bgset.min.js"
                     ></script>
@@ -180,7 +180,7 @@ class MyDocument extends Document {
                     <script
                         defer
                         src={`${process.env.NEXT_PUBLIC_BASE_ASSET}/js/main.js`}
-                    ></script>
+                    ></script> */}
                 </body>
             </Html>
         );
