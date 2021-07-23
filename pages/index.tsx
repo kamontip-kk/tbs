60b773ab99d5e2d5e563ad7370a76abfb48e1455
wndererk <bangwan.ap@hotmail.com>
2021-07-23 07:32:21 +0700

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,35 @@ import React from 'react';
 import Head from 'next/head'
 import { NextSeo } from 'next-seo';
 import { seo } from '../components/seo/home';
-// import Layout from '../components/Layout';
-// import HeroSection from '../components/home/HeroSection';
-// import PartnerSection from '../components/home/PartnerSection';
-// import FeatureSection from '../components/home/FeatureSection';
-// import ServiceSection from '../components/home/ServiceSection';
-// import SimpleSliderSection from '../components/home/SimpleSliderSection';
-// import GlobeSection from '../components/home/GlobeSection';
-// import SmsSection from '../components/home/SmsSection';
-// import TestimonialSection from '../components/home/TestimonialSection';
-// import EduSection from '../components/home/EduSection';
-// import CtaSection from '../components/home/CtaSection';
-// import BacktoTop from '../components/BacktoTop';
+import Layout from '../components/Layout';
+import HeroSection from '../components/home/HeroSection';
+import PartnerSection from '../components/home/PartnerSection';
+import FeatureSection from '../components/home/FeatureSection';
+import ServiceSection from '../components/home/ServiceSection';
+import SimpleSliderSection from '../components/home/SimpleSliderSection';
+import GlobeSection from '../components/home/GlobeSection';
+import SmsSection from '../components/home/SmsSection';
+import TestimonialSection from '../components/home/TestimonialSection';
+import EduSection from '../components/home/EduSection';
+import CtaSection from '../components/home/CtaSection';
+import BacktoTop from '../components/BacktoTop';
 import { useRouter } from 'next/router'
 import th from '../locales/th/Home.json';
 import en from '../locales/en/Home.json';
 //dynamicload
-import dynamic from 'next/dynamic'
-const Layout = dynamic(() => import('../components/Layout'))
-const HeroSection = dynamic(() => import('../components/home/HeroSection'))
-const PartnerSection = dynamic(() => import('../components/home/PartnerSection'))
-const FeatureSection = dynamic(() => import('../components/home/FeatureSection'))
-const ServiceSection = dynamic(()=> import('../components/home/ServiceSection'))
-const SimpleSliderSection = dynamic(() => import('../components/home/SimpleSliderSection'))
-const GlobeSection = dynamic(() => import('../components/home/GlobeSection'))
-const SmsSection = dynamic(() => import('../components/home/SmsSection'))
-const TestimonialSection = dynamic(() => import('../components/home/TestimonialSection'))
-const EduSection = dynamic(() => import('../components/home/EduSection'))
-const CtaSection = dynamic(() => import('../components/home/CtaSection'))
-const BacktoTop = dynamic(() => import('../components/BacktoTop'))
+// import dynamic from 'next/dynamic'
+// const Layout = dynamic(() => import('../components/Layout'))
+// const HeroSection = dynamic(() => import('../components/home/HeroSection'))
+// const PartnerSection = dynamic(() => import('../components/home/PartnerSection'))
+// const FeatureSection = dynamic(() => import('../components/home/FeatureSection'))
+// const ServiceSection = dynamic(()=> import('../components/home/ServiceSection'))
+// const SimpleSliderSection = dynamic(() => import('../components/home/SimpleSliderSection'))
+// const GlobeSection = dynamic(() => import('../components/home/GlobeSection'))
+// const SmsSection = dynamic(() => import('../components/home/SmsSection'))
+// const TestimonialSection = dynamic(() => import('../components/home/TestimonialSection'))
+// const EduSection = dynamic(() => import('../components/home/EduSection'))
+// const CtaSection = dynamic(() => import('../components/home/CtaSection'))
+// const BacktoTop = dynamic(() => import('../components/BacktoTop'))
 
 const Homepage = () => {
   const router = useRouter();
