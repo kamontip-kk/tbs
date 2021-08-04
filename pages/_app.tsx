1e8ba66881693370cda870a005127ae14fae055a
wndererk <bangwan.ap@hotmail.com>
2021-08-05 00:11:31 +0700

    check: responsive css (not complete)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,8 @@ import { DefaultSeo } from 'next-seo';
 import { seo } from '../components/seo/defaultseo';
 import {useRouter} from 'next/router'
 import Proloader from '../components/Proloader';
-import '../public/assets/css/style.css'
-import '../public/assets/css/responsive.css'
+// import '../public/assets/css/style.css'
+// import '../public/assets/css/responsive.css'
 import '../public/assets/css/globals.css'
 import '../public/assets/css/backtotop.css'
 import '../public/assets/css/proloader.css'
